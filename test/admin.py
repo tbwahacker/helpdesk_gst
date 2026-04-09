f85ba7cfbe2c1e1db2e1d9c90108b5566fd5c7bd
Vee-192 <veronicammbaga192@gmail.com>
2026-04-09 14:16:37 +0300

    columns added (#2)

--- test/admin.py
+++ test/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from test.models import Test
+
 # Register your models here.
+admin.site.register(Test)
--- test/models.py
+++ test/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    column1=models.TextField()
+    column2=models.IntegerField(default=0)
+    column3=models.CharField(unique=True, null=False)
+    column4=models.DateField(auto_now=True)
