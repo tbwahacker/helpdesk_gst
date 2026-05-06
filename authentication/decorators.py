9f0ce721bb061d5483a3e060c39e2e33ab53d246
Diana-0743 <dianachristine2004@gmail.com>
2026-05-06 10:45:07 +0300

    Diana (#18)
    * added form
    
    * login issue
    
    * user's dashboard
    
    * staff dashboard

--- authentication/decorators.py
+++ authentication/decorators.py
@@ -1,16 +1,29 @@
-from django.http import HttpResponseForbidden
+from django.shortcuts import redirect
+from functools import wraps
+
 
 def admin_required(view_func):
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        if not request.user.is_superuser:
-            return HttpResponseForbidden("Admins only")
-        return view_func(request, *args, **kwargs)
+        if request.user.is_authenticated and request.user.role == 'admin':
+            return view_func(request, *args, **kwargs)
+        return redirect('login')
     return wrapper
 
 
 def staff_required(view_func):
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        if not request.user.is_staff:
-            return HttpResponseForbidden("Staff only")
-        return view_func(request, *args, **kwargs)
+        if request.user.is_authenticated and request.user.role == 'staff':
+            return view_func(request, *args, **kwargs)
+        return redirect('login')
+    return wrapper
+
+
+def user_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.role == 'user':
+            return view_func(request, *args, **kwargs)
+        return redirect('login')
     return wrapper
--- core_business/urls.py
+++ core_business/urls.py
@@ -5,5 +5,6 @@ from core_business import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ticket', views.ticket, name='ticket'),
-    path('account/', views.account_view, name='account')
+    path('account/', views.account_view, name='account'),
+    path('assigned/', views.assigned_view, name='assigned')
 ]
