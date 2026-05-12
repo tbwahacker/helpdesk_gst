40582177d0b0a5a08ef3b3e3f9dae5869eb9c16f
Diana-0743 <dianachristine2004@gmail.com>
2026-05-12 14:59:10 +0300

    Diana (#23)
    * added form
    
    * login issue
    
    * user's dashboard
    
    * staff dashboard
    
    * requirements.txt
    
    * fixed the staff issue
    
    * ticket counts

--- authentication/views.py
+++ authentication/views.py
@@ -9,8 +9,6 @@ from authentication.decorators import admin_required, staff_required
 from django.contrib.auth.decorators import login_required
 from core_business.models import Ticket
 
-from core_business.models import Ticket
-
 
 @login_required
 @admin_required
